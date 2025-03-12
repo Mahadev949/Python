ef298989194f8000dbf2b26699c9ccf5c94f95a6
Mahadev949 <167230806+Mahadev949@users.noreply.github.com>
2025-03-12 16:15:02 +0530

    Create Season.py

---
+++ Season.py
@@ -0,0 +1,9 @@
+a=int(input())
+if a==12 or a==1 or a==2:
+    print("Winter Season")
+elif 3<=a<=5:
+    print("Summer Season")
+elif 6<=a<=9:
+    print("Rainy Season")
+elif 10<=a<=11:
+    print("Autumn Season")
